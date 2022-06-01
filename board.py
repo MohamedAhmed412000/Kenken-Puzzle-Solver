7398e414ca621b7c09757c3ceebd10bec06923de
MohamedAhmed412000 <memoahmed412000@gmail.com>
2022-06-01 17:02:42 +0200

    Convert to class Board

--- board.py
+++ board.py
@@ -1,111 +1,162 @@
 import pygame
-from Input.input import N, grid
+import os
 
-WIDTH = 550
-BG_COLOR = (251, 247, 245)
-LINE_COLOR = (0, 0, 0)
-NUM_COLOR = (10, 10, 10)
-LOWER_NUM_COLOR = (52, 31, 151)
-MARGIN = (WIDTH - 100) / N
-BOARD = [[0 for i in range(N)] for j in range(N)]
+class Board:
+    def __init__(self, solver = 'user', n = 3, algorithm = 'BT'):
+        self.solver = solver
+        self.N = n
+        self.algorithm = algorithm
+        self.WIDTH = 550
+        self.BG_COLOR = (251, 247, 245)
+        self.LINE_COLOR = (0, 0, 0)
+        self.NUM_COLOR = (10, 10, 10)
+        self.LOWER_NUM_COLOR = (52, 31, 151)
+        self.MARGIN = (self.WIDTH - 100) / self.N
+        self.BOARD = [[0 for _ in range(self.N)] for _ in range(self.N)]
+        self.continous = False
+        
+    def set_inputs(self, filepath: str):
+        OP = {'add': '+', 'sub': '-', 'mul': '*', 'div': '\\', "''": ''}
+        fh = open(f'Input/{filepath}', 'r')
+        lines = fh.readlines()
+        fh.close()
+        self.N = int(lines[0])
+        self.grid = []
+        for line in lines[1:]:
+            k, op, v = line.strip().split(' ')
+            v = f'{v}{OP[op]}'
+            cells = k[1:-1].replace('),(', '-').replace('(', '').replace(')', '').split('-')
+            vals = []
+            for cell in cells:
+                x, y = cell.split(',')
+                val = int(x) * self.N + int(y) + 1
+                vals.append(val)
+            self.grid.append([v, vals])
+        self.MARGIN = (self.WIDTH - 100) / self.N
+        self.BOARD = [[0 for i in range(self.N)] for j in range(self.N)]
+        return self.N, self.grid
 
-def coordinates(x, y):
-    return ((50+(x-1)*MARGIN, 50+(y-1)*MARGIN), (50+(x-1)*MARGIN, 50+y*MARGIN)), \
-            ((50+x*MARGIN, 50+(y-1)*MARGIN), (50+x*MARGIN, 50+y*MARGIN)), \
-            ((50+(x-1)*MARGIN, 50+(y-1)*MARGIN), (50+x*MARGIN, 50+(y-1)*MARGIN)), \
-            ((50+(x-1)*MARGIN, 50+y*MARGIN), (50+x*MARGIN, 50+y*MARGIN))
+    def get_inputs(self, grid):
+        self.grid = grid
 
-def clear(win, x, y):
-    if x < 0 or x >= N or y < 0 or y >= N:
+    def coordinates(self, x, y):
+        return ((50+(x-1)*self.MARGIN, 50+(y-1)*self.MARGIN), (50+(x-1)*self.MARGIN, 50+y*self.MARGIN)),\
+                ((50+x*self.MARGIN, 50+(y-1)*self.MARGIN), (50+x*self.MARGIN, 50+y*self.MARGIN)),\
+                ((50+(x-1)*self.MARGIN, 50+(y-1)*self.MARGIN), (50+x*self.MARGIN, 50+(y-1)*self.MARGIN)),\
+                ((50+(x-1)*self.MARGIN, 50+y*self.MARGIN), (50+x*self.MARGIN, 50+y*self.MARGIN))
+
+    
+    def clear(self, win, x, y):
+        if x < 0 or x >= self.N or y < 0 or y >= self.N:
+            return
+        pygame.draw.rect(win, self.BG_COLOR, (50+(x+0.5)*self.MARGIN, 50+(y+0.4)*self.MARGIN, 0.5*self.MARGIN-10, 0.6*self.MARGIN-10))
+        pygame.display.update()
         return
-    pygame.draw.rect(win, BG_COLOR, (50+(x+0.5)*MARGIN, 50+(y+0.4)*MARGIN, 0.5*MARGIN-10, 0.6*MARGIN-10))
-    pygame.display.update()
-    return
 
-def insert(win, x, y):
-    if x < 0 or x >= N or y < 0 or y >= N:
-        return
-    FONT = pygame.font.SysFont("Verdana", (10-N)*5+7)
-    # 6 -> 27
-    # 5 -> 32
-    # 4 -> 37
-    # 3 -> 42
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                if 0 < event.key - 48 < N + 1:
-                    BOARD[y][x] = event.key - 48
-                    pygame.draw.rect(win, BG_COLOR, (50+(x+0.5)*MARGIN, 50+(y+0.4)*MARGIN, 0.5*MARGIN-10, 0.6*MARGIN-10))
-                    text = FONT.render(str(BOARD[y][x]), True, LOWER_NUM_COLOR)
-                    win.blit(text, (50+MARGIN*(x+0.5), 50+MARGIN*(y+0.35)))
-                    pygame.display.update()
-                    print(BOARD)
+    def insert(self, win, x, y):
+        if x < 0 or x >= self.N or y < 0 or y >= self.N:
+            return
+        FONT = pygame.font.SysFont("Verdana", (10-self.N)*5+7)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     return
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if event.button == 1:
-                    insert(win, int((pos[0]-50)//MARGIN), int((pos[1]-50)//MARGIN))
-                if event.button == 3:
-                    clear(win, int((pos[0]-50)//MARGIN), int((pos[1]-50)//MARGIN))
+                if event.type == pygame.KEYDOWN:
+                    if 0 < event.key - 48 < self.N + 1:
+                        self.BOARD[y][x] = event.key - 48
+                        pygame.draw.rect(win, self.BG_COLOR, (50+(x+0.5)*self.MARGIN, 50+(y+0.4)*self.MARGIN, 0.5*self.MARGIN-10, 0.6*self.MARGIN-10))
+                        text = FONT.render(str(self.BOARD[y][x]), True, self.LOWER_NUM_COLOR)
+                        win.blit(text, (50+self.MARGIN*(x+0.5), 50+self.MARGIN*(y+0.35)))
+                        pygame.display.update()
+                        # print(self.BOARD)
+                        if self.soln == self.BOARD:
+                            print('game Solved')
+                            return True
+                        return False
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    if event.button == 1:
+                        return self.insert(win, int((pos[0]-50)//self.MARGIN), int((pos[1]-50)//self.MARGIN))
+                    if event.button == 3:
+                        self.clear(win, int((pos[0]-50)//self.MARGIN), int((pos[1]-50)//self.MARGIN))
 
-
-def main(grid):
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, WIDTH))
-    pygame.display.set_caption("Kenken")
-    screen.fill(BG_COLOR)
-    FONT = pygame.font.SysFont("Comic Sans MS", (10-N)*5)
-    # 6 -> 20
-    # 5 -> 25
-    # 4 -> 30
-    # 3 -> 35
-
-    # grid lines
-    for i in range(N+1):
-        width = 2
-        if i % N == 0:
-            width = 4
-        pygame.draw.line(screen, LINE_COLOR, (50+MARGIN*i, 50), (50+MARGIN*i, 500), width)
-        pygame.draw.line(screen, LINE_COLOR, (50, 50+MARGIN*i), (500, 50+MARGIN*i), width)
-
-    # grid
-    border = {}
-    for item in grid:
-        dimensions = {}
-        for i in item[1]:
-            l, r, n, s = coordinates((i-1)%N+1, (i-1)//N+1)
-            for element in [l, r, n, s]:
-                if element not in dimensions.keys():
-                    dimensions[element] = 1
-                else:
-                    dimensions[element] += 1
-            if i == min(item[1]):
-                text = FONT.render(item[0], True, NUM_COLOR)
-                screen.blit(text, (50+MARGIN*((i-1)%N) + 7, 50+MARGIN*((i-1)//N)))
-        for k, v in dimensions.items():
-            if v == 1:
-                border[k] = dimensions[k]
-
-    for k, v in border.items():
-        if v == 1:
-            pygame.draw.line(screen, LINE_COLOR, k[0], k[1], 5)
-
-    pygame.display.update()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            if event.type == pygame.WINDOWRESTORED:
+    def close(self):
+        pygame.quit()
+    
+    def fill_board(self, win, soln):
+        FONT = pygame.font.SysFont("Verdana", (10-self.N)*5+7)
+        for i in range(len(soln)):
+            for j in range(len(soln[i])):
+                text = FONT.render(str(soln[i][j]), True, self.LOWER_NUM_COLOR)
+                win.blit(text, (50+self.MARGIN*(j+0.5), 50+self.MARGIN*(i+0.35)))
                 pygame.display.update()
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if event.button == 1:
-                    insert(screen, int((pos[0]-50)//MARGIN), int((pos[1]-50)//MARGIN))
-                if event.button == 3:
-                    clear(screen, int((pos[0]-50)//MARGIN), int((pos[1]-50)//MARGIN))
+    
+    def set_output(self, soln):
+        self.soln = soln
+                
+    def run(self, soln = ''):
+        self.close()
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "700,100"
+        pygame.init()
+        screen = pygame.display.set_mode((self.WIDTH, self.WIDTH))
+        pygame.display.set_caption("Kenken")
+        screen.fill(self.BG_COLOR)
+        FONT = pygame.font.SysFont("Comic Sans MS", (10-self.N)*5)
+        # grid lines
+        for i in range(self.N+1):
+            width = 2
+            if i % self.N == 0:
+                width = 4
+            pygame.draw.line(screen, self.LINE_COLOR, (50+self.MARGIN*i, 50), (50+self.MARGIN*i, 500), width)
+            pygame.draw.line(screen, self.LINE_COLOR, (50, 50+self.MARGIN*i), (500, 50+self.MARGIN*i), width)
+        # grid
+        border = {}
+        for item in self.grid:
+            dimensions = {}
+            for i in item[1]:
+                l, r, n, s = self.coordinates((i-1)%self.N+1, (i-1)//self.N+1)
+                for element in [l, r, n, s]:
+                    if element not in dimensions.keys():
+                        dimensions[element] = 1
+                    else:
+                        dimensions[element] += 1
+                if i == min(item[1]):
+                    text = FONT.render(item[0], True, self.NUM_COLOR)
+                    screen.blit(text, (50+self.MARGIN*((i-1)%self.N) + 7, 50+self.MARGIN*((i-1)//self.N)))
+            for k, v in dimensions.items():
+                if v == 1:
+                    border[k] = dimensions[k]
 
-main(grid)
+        for k, v in border.items():
+            if v == 1:
+                pygame.draw.line(screen, self.LINE_COLOR, k[0], k[1], 5)
+        self.continous = True
+
+        pygame.display.update()
+
+        if soln != '':
+            self.fill_board(screen, soln)
+
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.WINDOWRESTORED:
+                    pygame.display.update()
+                if event.type == pygame.MOUSEBUTTONUP and self.continous:
+                    if self.solver == 'user':
+                        pos = pygame.mouse.get_pos()
+                        if event.button == 1:
+                            check = self.insert(screen, int((pos[0]-50)//self.MARGIN), int((pos[1]-50)//self.MARGIN))
+                            if check:
+                                FONT = pygame.font.SysFont("Verdana", 23)
+                                text = FONT.render("Game Solved", True, (255, 0, 0))
+                                screen.blit(text, (200, 10))
+                                pygame.display.update()
+                                self.continous = False
+                        if event.button == 3:
+                            self.clear(screen, int((pos[0]-50)//self.MARGIN), int((pos[1]-50)//self.MARGIN))
+
+
