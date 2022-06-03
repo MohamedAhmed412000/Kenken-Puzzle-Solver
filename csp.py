1ad2c8bacaad67ac6ccd7758462da96318e7213e
MostafaAkrsh <mostafasamir355@gmail.com>
2022-06-03 10:16:56 +0200

    Update csp.py

--- csp.py
+++ csp.py
@@ -107,3 +107,90 @@ class CSP(search.Problem):
         "Return a list of variables in current assignment that are in conflict"
         return [var for var in self.variables
                 if self.nconflicts(var, current[var], current) > 0]
+    
+    def AC3(csp, queue=None, removals=None):
+    """[Figure 6.3]"""
+    if queue is None:
+        queue = [(Xi, Xk) for Xi in csp.variables for Xk in csp.neighbors[Xi]]
+    csp.support_pruning()
+    while queue:
+        (Xi, Xj) = queue.pop()
+        if revise(csp, Xi, Xj, removals):
+            if not csp.curr_domains[Xi]:
+                return False
+            for Xk in csp.neighbors[Xi]:
+                if Xk != Xi:
+                    queue.append((Xk, Xi))
+        return True
+
+
+    def revise(csp, Xi, Xj, removals):
+        "Return true if we remove a value."
+        revised = False
+        for x in csp.curr_domains[Xi][:]:
+            # If Xi=x conflicts with Xj=y for every possible y, eliminate Xi=x
+            if all(not csp.constraints(Xi, x, Xj, y) for y in csp.curr_domains[Xj]):
+                csp.prune(Xi, x, removals)
+                revised = True
+        return revised
+
+    # ______________________________________________________________________________
+    # CSP Backtracking Search
+
+    # Variable ordering
+
+
+    def first_unassigned_variable(assignment, csp):
+        "The default variable order."
+        return first([var for var in csp.variables if var not in assignment])
+
+
+    def mrv(assignment, csp):
+        "Minimum-remaining-values heuristic."
+        return argmin_random_tie(
+            [v for v in csp.variables if v not in assignment],
+            key=lambda var: num_legal_values(csp, var, assignment))
+
+
+    def num_legal_values(csp, var, assignment):
+        #if csp.curr_domains:
+            #return len(csp.curr_domains[var])
+        #else:
+            return count(csp.nconflicts(var, val, assignment) == 0
+                         for val in csp.domains[var])
+
+    # Value ordering
+
+
+    def unordered_domain_values(var, assignment, csp):
+        "The default value order."
+        return csp.choices(var)
+
+
+    def lcv(var, assignment, csp):
+        "Least-constraining-values heuristic."
+        return sorted(csp.choices(var),
+                      key=lambda val: csp.nconflicts(var, val, assignment))
+
+    # Inference
+
+
+    def no_inference(csp, var, value, assignment, removals):
+        return True
+
+
+    def forward_checking(csp, var, value, assignment, removals):
+        "Prune neighbor values inconsistent with var=value."
+        for B in csp.neighbors[var]:
+            if B not in assignment:
+                for b in csp.curr_domains[B][:]:
+                    if not csp.constraints(var, value, B, b):
+                        csp.prune(B, b, removals)
+                if not csp.curr_domains[B]:
+                    return False
+        return True
+
+
+    def mac(csp, var, value, assignment, removals):
+        "Maintain arc consistency."
+        return AC3(csp, [(X, var) for X in csp.neighbors[var]], removals)
